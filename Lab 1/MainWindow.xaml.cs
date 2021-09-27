88aa9bb0d4f68bb32e05e9680f8e046e2a2f9ba6
Lucas Smith <lsoceanengineer@gmail.com>
2021-09-26 18:24:54 -0700

    Add project files.

---
+++ Lab 1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace Lab_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstName.Text;
+            string lastName = LastName.Text;
+
+            string pigFirst;
+            string pigLast;
+
+            firstName = firstName.ToLower();
+            lastName = lastName.ToLower();
+            int firstLength = firstName.Length;
+            int lastLength = lastName.Length;
+
+            pigFirst = firstName.Substring(1, firstLength - 1) + firstName.Substring(0, 1) + "ay";
+            int pigFirstLength = pigFirst.Length;
+            pigFirst = pigFirst.Substring(0, 1).ToUpper() + pigFirst.Substring(1, pigFirstLength - 1);
+            pigLast = lastName.Substring(1, lastLength - 1) + lastName.Substring(0, 1) + "ay";
+            int pigLastLength = pigLast.Length;
+            pigLast = pigLast.Substring(0, 1).ToUpper() + pigLast.Substring(1, pigLastLength - 1);
+
+            Output.Text = pigFirst + " " + pigLast;
+        }
+    }
+}
---
+++ Lab 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Does your lamp work?");
+            string lampBroken = Console.ReadLine();
+            lampBroken = lampBroken.ToLower();
+            if (lampBroken == "yes")
+            {
+                Console.WriteLine("Is the lamp plugged in?");
+                string plugLamp = Console.ReadLine();
+                plugLamp = plugLamp.ToLower();
+                if (plugLamp == "no")
+                {
+                    Console.WriteLine("Plug in your lamp.");
+                } else if (plugLamp == "yes")
+                {
+                    Console.WriteLine("Is the bulb burned out?");
+                    string bulbBurned = Console.ReadLine();
+                    bulbBurned = bulbBurned.ToLower();
+                    if (bulbBurned == "yes")
+                    {
+                        Console.WriteLine("Replace the bulb.");
+                    } else if (bulbBurned == "no")
+                    {
+                        Console.WriteLine("Repair the lamp.");
+                    }
+                }
+            } else
+            {
+                Console.WriteLine("That's not a valid input. Please answer yes or no.");
+            }
+
+        }
+    }
+}
